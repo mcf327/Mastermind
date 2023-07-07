eac95abce862de9b7fcf195da689c240d4b406e4
mcf327 <mcf327@gmail.com>
2023-07-07 09:42:12 -0400

    most logic complete; some style improvements

--- main.js
+++ main.js
@@ -7,17 +7,21 @@ const MAX_GUESSES = 10;  // 10 tries to get it right!
 let guesses = 10; // keep track of player guesses and use to keep track of corresponding/current row; if this reaches 10 player loses
 let randomCode = []; // variable that will hold computer-generated code for player to guess
 let currentHoleIdx = 0; // index of hole in current row to be filled with clicked color
+let gameOver = false;
 
 // cached elements
 const secretRow = document.getElementById('secret-row');
 const guessRows = Array.from(document.querySelectorAll('.row'));
 const colorOptions = Array.from(document.getElementById('player-choices').querySelectorAll('.hole'));
 const checkGuessButton = document.getElementById('check-guess');
+const clearGuessButton = document.getElementById('clear-guess');
 const newGameButton = document.getElementById('new-game');
+const message = document.getElementById('message');
 
 // event listeners
 newGameButton.addEventListener('click', init);
 checkGuessButton.addEventListener('click', checkGuess);
+clearGuessButton.addEventListener('click', clearGuess);
 colorOptions.forEach((option, idx) => {
     option.addEventListener('click', renderGuess);
 });
@@ -36,48 +40,55 @@ function getRandomCode() {
 
 // check player guess againt randomCode and award appropriate number of red/white pegs
 function checkGuess() {
+    if (gameOver) {
+        return;
+    }
+    currentHoleIdx = 0;
     const currentRow = guessRows[guesses];
     const guessArray = Array.from(currentRow.querySelectorAll('.hole'));
     let redPegs = 0;
     let whitePegs = 0;
     let correctGuess = true;
+    let codeCopy = [...randomCode];
     for (let i = 0; i < guessArray.length; i++) {
         const color = getComputedStyle(guessArray[i]).backgroundColor;
-        if (color !== randomCode[i]) {
+        if (color !== codeCopy[i]) {
           correctGuess = false;
         } else {
             redPegs++;
+            codeCopy[i] = null;
         }
     }
-
     if (correctGuess === true) {
-        displayWin();
+        win();
         return;
     }
-
     for(let i = 0; i < guessArray.length; i++) {
         const color = getComputedStyle(guessArray[i]).backgroundColor;
-        for (let j = 0; j < guessArray.length; j++) {
-            if (color === randomCode[j] && i !== j) {
-                whitePegs++;
-                break;
-            }
+        if (color !== codeCopy[i] && codeCopy.includes(color)) {
+            whitePegs++;
+            codeCopy[codeCopy.indexOf(color)] = null;
         }
     }
-
     renderFeedback(redPegs, whitePegs, currentRow);
     guesses--;
     if(guesses === 0) {
-        displayLose();
+        lose();
+    } else {
+        highlightRow(guesses);
     }
 }
 
 // function to initialize the game
 function init() {
-    guesses = 10;
+    gameOver = false;
+    guesses = MAX_GUESSES;
     randomCode = getRandomCode();
-    //clear the board:
+    message.innerText = "Welcome to Mastermind! Try to guess the code!";
+    currentHoleIdx = 0;
+    // clear the board:
     guessRows.forEach((row) => {
+        row.classList.remove('highlight');
         const holes = Array.from(row.querySelectorAll('.hole'));
         holes.forEach((hole) => {
             hole.style.backgroundColor = 'gray';
@@ -87,17 +98,21 @@ function init() {
             hole.style.backgroundColor = 'gray';
         });
     });
+    guessRows[guesses].classList.add('highlight');
 }
 
 function renderGuess(evt) {
+    if (gameOver) {
+        return;
+    }
     const currentRow = guessRows[guesses];
     const clickedColor = getComputedStyle(evt.target).backgroundColor; // get clicked color
     const currentHole = currentRow.querySelectorAll('.hole')[currentHoleIdx];
     currentHole.style.backgroundColor = clickedColor;
     currentHoleIdx++;
-    if(currentHoleIdx === CODE_LENGTH) {
+    /* if(currentHoleIdx === CODE_LENGTH) {
         currentHoleIdx = 0;
-    }
+    } */
 }
 
 function renderFeedback(redPegs, whitePegs, currentRow) {
@@ -113,41 +128,40 @@ function renderFeedback(redPegs, whitePegs, currentRow) {
     });
 }
 
-function displayWin() {
-    const msg = document.getElementById('message');
-    msg.innerText = 'Congrats! You win!'
+function revealCode() {
+    const code = Array.from(secretRow.querySelectorAll('.hole'));
+    for (let i = 0; i < CODE_LENGTH; i++) {
+        const hole = code[i];
+        hole.style.backgroundColor = randomCode[i];
+        hole.textContent = '';
+    }
 }
 
-function displayLose() {
-    const msg = document.getElementById('message');
-    msg.innerText = 'You lose! Better luck next time!';
+function win() {
+    message.innerText = 'Congrats! You win!'
+    revealCode();
+    gameOver = true;
+}
+
+function lose() {
+    message.innerText = 'You lose! Better luck next time!';
+    revealCode();
+    gameOver = true;
+}
+
+function highlightRow(guesses) {
+    const lastRow = guessRows[guesses +1]; 
+    const currentRow = guessRows[guesses];
+    lastRow.classList.remove('highlight');
+    currentRow.classList.add('highlight');
+}
+
+function clearGuess() {
+    currentHoleIdx = 0;
+    const guess = Array.from(guessRows[guesses].querySelectorAll('.hole'));
+    guess.forEach((hole) => {
+        hole.style.backgroundColor = 'gray';
+    });
 }
 
 init();
-
-/* // constants 
-- colors array for six possible player color choices
-- codeLength = 4
-- maxGuesses = 10
--function randomCode() to generate the random code for player to guess. Iterate over colors array for i < codeLength, choose random color and push it to new array, return the new array.
--function checkGuess() to check player’s guess. Inside this function declare two variables (redPegs, whitePegs) to keep track of how many red/white “feedback” pegs will be awarded for the guess. Should have two nested functions:
-        -function checkRed() to check for red pegs (correct color/correct position). Compare  random code array to array for player’s current guess. If guess array[i] === random array [i], increment red pegs +1.
-        -function checkWhite() to check for white pegs (correct color/wrong position). If correct color/wrong position found, increment white pegs +1. // Still have to come back and work out the rest of the logic here
-return redPegs, whitePegs
--function init() that will set guess to 0 and call a function renderBoard() to display new game board
--function renderBoard() to display game board with previous/current guesses
--function renderFeedback() to display feedback pegs in right hand column
--function getGuess() - get user input and assign to guessArray
--function displayWinLose() to display a winning message if player guesses correctly or a losing message if guess > maxGuesses
--event listeners for buttons and user input - try to implement drag and drop for player guess
-—-------------------------------------
-let guesses = 0;
-let guessArray; // array for player guess 
-while guesses < maxGuesses
-        renderBoard()
-        guessArray = getGuess()
-        checkGuess(guessArray, randomCode)
-        If guessArray === randomCode, display message and break while loop - player wins
-If guesses === maxGuesses, display message and break loop - player loses
-guesses++
-init(); */
